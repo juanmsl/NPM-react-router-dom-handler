bccb0dc23a40983e0075252ec557f5ca15150413
Juan Sanchez <juanmsl_pk@hotmail.com>
2021-03-01 11:48:54 -0500

    Updates

---
+++ precache-manifest.4116416db20ec24e60bb7f77f893fcf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a84aab2260c3ee7d6730b2cb363b12a",
+    "url": "/react-router-dom-handler/index.html"
+  },
+  {
+    "revision": "eb725e23290123d002bd",
+    "url": "/react-router-dom-handler/static/css/main.222b3809.chunk.css"
+  },
+  {
+    "revision": "2f69b35b6ee5830688c6",
+    "url": "/react-router-dom-handler/static/js/2.e405730b.chunk.js"
+  },
+  {
+    "revision": "eb725e23290123d002bd",
+    "url": "/react-router-dom-handler/static/js/main.93241462.chunk.js"
+  },
+  {
+    "revision": "7ec82eaa59384bd0aec7",
+    "url": "/react-router-dom-handler/static/js/runtime-main.5a5d4aa6.js"
+  }
+]);
